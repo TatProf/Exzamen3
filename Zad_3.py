a7072d899436275c710d286cd353eda2a734aec8
Tatien <misenko.t@gmail.com>
2022-07-19 23:26:59 +0300

    add name logic to Zad_3.py

---
+++ Zad_3.py
@@ -0,0 +1,120 @@
+# 3. Решите задачу
+# Класс Tomato:
+# 1. Создайте класс Tomato
+# 2. Создайте статическое свойство states, которое будет содержать все стадии
+# созревания помидора
+# 3. Создайте метод __init__(), внутри которого будут определены два динамических
+# protected свойства: 1) _index - передается параметром и 2) _state - принимает первое
+# значение из словаря states
+# 4. Создайте метод grow(), который будет переводить томат на следующую стадию
+# созревания
+# 5. Создайте метод is_ripe(), который будет проверять, что томат созрел (достиг
+# последней стадии созревания)
+
+# Класс TomatoBush
+# 1. Создайте класс TomatoBush
+# 2. Определите метод __init__(), который будет принимать в качестве параметра
+# количество томатов и на его основе будет создавать список объектов класса
+# Tomato. Данный список будет храниться внутри динамического свойства tomatoes.
+# 3. Создайте метод grow_all(), который будет переводить все объекты из списка
+# томатов на следующий этап созревания
+# 4. Создайте метод all_are_ripe(), который будет возвращать True, если все томаты из
+# списка стали спелыми
+# 5. Создайте метод give_away_all(), который будет чистить список томатов после
+# сбора урожая
+# Класс Gardener
+# 1. Создайте класс Gardener
+# 2. Создайте метод __init__(), внутри которого будут определены два динамических
+# свойства: 1) name - передается параметром, является публичным и 2) _plant -
+# принимает объект класса TomatoBush, является protected
+# 3. Создайте метод work(), который заставляет садовника работать, что позволяет
+# растению становиться более зрелым
+# 4. Создайте метод harvest(), который проверяет, все ли плоды созрели. Если все -
+# садовник собирает урожай. Если нет - метод печатает предупреждение.
+# 5. Создайте статический метод knowledge_base(), который выведет в консоль справку
+# по садоводству.
+
+class Tomato:
+    states = [10, 20, 30]
+
+    def __init__(self,ind):
+        self._index = ind
+        self._state = self.states[ind]
+
+    def grow(self):
+        if self.is_ripe():
+            return
+        self._index +=1
+        self._state = self.states[self._index]
+
+    def is_ripe(self):
+        if self._state == self.states[-1]:
+            return True
+        else:
+            return False
+
+class TomatoBush:
+    def __init__(self,kol):
+        self.tomatoes = []
+        for i in range(kol):
+            t = Tomato(0)
+            self.tomatoes.append(t)
+
+
+    def grow_all(self):
+        for i in self.tomatoes:
+            i.grow()
+
+    def all_are_ripe(self):
+        for i in self.tomatoes:
+            if not i.is_ripe():
+                return False
+        return True
+
+    def give_away_all(self):
+        self.tomatoes.clear()
+
+
+class Gardener:
+
+    def __init__(self,name,plant):
+        self.name = name
+        self._plant = plant
+
+    def work(self):
+        self._plant.grow_all()
+
+    def harvest(self):
+        if self._plant.all_are_ripe():
+            print(" томаты собраны ")
+            return (self._plant.give_away_all())
+        else:
+            print(" ждите ")
+
+
+
+
+
+# Тесты:
+# 1. Вызовите  справку по садоводству
+# 2. Создайте объекты классов TomatoBush и Gardener
+# 3. Используя объект класса Gardener, поухаживайте за кустом с помидорами
+# 4. Попробуйте собрать урожай
+# 5. Если томаты еще не дозрели, продолжайте ухаживать за ними
+# 6. Соберите урожай
+
+t = TomatoBush(5)
+r =Gardener ('Niki',t)
+r.work()
+r.harvest()
+while not t.all_are_ripe():
+    r.work()
+r.harvest()
+
+
+
+
+
+
+
+
